dc00d1a6b0277c7324c08f88a72b661d912a1de9
Wesley <wesleystedman@gmail.com>
2020-10-12 13:27:53 -0700

    Initialize Express server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+const app = express();
+
+require('dotenv').config();
+// require('./config/database');
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Unprotected API routes
+// app.use('/api/users', require('./routes/api/users'));
+
+// Mount auth middleware
+// app.use(require('./config/auth'));
+
+// Protected API routes
+// app.use('/api/scores', require('./routes/api/scores'));
+
+// "catch all" route
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
