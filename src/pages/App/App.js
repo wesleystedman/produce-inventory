8b507200be262912045c992c905d5187544c1b89
Wesley <wesleystedman@gmail.com>
2020-10-12 19:05:18 -0700

    Start client-side implementation of auth Borrowing heavily from the react mastermind project

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from '../../logo.svg';
+import React, { Component } from 'react';
+import { NavLink, Route, Switch } from 'react-router-dom';
 import './App.css';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
+  
+  handleSignupOrLogin = () => {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <NavLink to="/" exact>&nbsp;Home&nbsp;</NavLink>
+          <NavLink to="/signup" exact>&nbsp;Sign Up&nbsp;</NavLink>
+          <NavLink to="/login" exact>&nbsp;Log In&nbsp;</NavLink>
+        </nav>
+        <h1>placeholder text</h1>
+        <Switch>
+          <Route exact path='/' render={() =>
+            <p>root</p>
+          } />
+          <Route exact path='/signup' render={({ history }) =>
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          } />
+          <Route exact path='/login' render={({ history }) =>
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
