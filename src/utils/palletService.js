46a7993ef9361698375a3e68db5f07944cce64b7
Wesley <wesleystedman@gmail.com>
2020-10-15 19:58:00 -0700

    Add util to access pallets API

---
+++ src/utils/palletService.js
@@ -0,0 +1,49 @@
+import tokenService from './tokenService';
+
+const BASE_URL = '/api/pallets/';
+
+export default {
+    getAll,
+    create,
+    update,
+    deleteOne,
+};
+
+function getAll() {
+    return fetch(BASE_URL).then(res => res.json());
+}
+
+function create(pallet) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer ' + tokenService.getToken()
+        },
+        body: JSON.stringify(pallet)
+    };
+    return fetch(BASE_URL, options).then(res => res.json());
+}
+
+function update(pallet) {
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer ' + tokenService.getToken()
+        },
+        body: JSON.stringify(pallet)
+    };
+    return fetch((BASE_URL + pallet.id), options).then(res => res.json());
+}
+
+function deleteOne(id) {
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer ' + tokenService.getToken()
+        }
+    };
+    return fetch((BASE_URL + id), options).then(res => res.json());
+}
