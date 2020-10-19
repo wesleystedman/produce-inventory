07b66527ee14bb9a55a1f6721ffc5efc20217089
Wesley <wesleystedman@gmail.com>
2020-10-19 03:00:02 -0700

    Make NavBar component code style consistent with other functional components

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
-const NavBar = (props) => {
-  return (
-    <nav>
-      {
-        props.user ?
-          <>
-            <NavLink to="/" exact>View Inventory</NavLink>
-            <NavLink to="/add" exact>Add Pallet</NavLink>
-            <Link to='' onClick={props.handleLogout}>&nbsp;Log Out&nbsp;</Link>
-            <span>&nbsp;Welcome {props.user.name}&nbsp;</span>
-          </>
-          :
-          <>
-            <NavLink to="/signup" exact>&nbsp;Sign Up&nbsp;</NavLink>
-            <NavLink to="/login" exact>&nbsp;Log In&nbsp;</NavLink>
-          </>
-      }
-    </nav>
-  );
-}
+const NavBar = (props) => (
+  <nav>
+    {
+      props.user ?
+        <>
+          <NavLink to="/" exact>View Inventory</NavLink>
+          <NavLink to="/add" exact>Add Pallet</NavLink>
+          <Link to='' onClick={props.handleLogout}>&nbsp;Log Out&nbsp;</Link>
+          <span>&nbsp;Welcome {props.user.name}&nbsp;</span>
+        </>
+        :
+        <>
+          <NavLink to="/signup" exact>&nbsp;Sign Up&nbsp;</NavLink>
+          <NavLink to="/login" exact>&nbsp;Log In&nbsp;</NavLink>
+        </>
+    }
+  </nav>
+);
 
 export default NavBar;
