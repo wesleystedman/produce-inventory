6ac5aa576ef5e5d1fa8a0529a750fd7f9c47ceec
Wesley <wesleystedman@gmail.com>
2020-10-19 00:47:02 -0700

    Add initial pallet display

---
+++ src/components/PalletCard/PalletCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PalletCard = (props) => (
+    <div>
+        {props.pallet.palletID}
+    </div>
+);
+
+export default PalletCard;
--- src/pages/PalletListPage/PalletListPage.jsx
+++ src/pages/PalletListPage/PalletListPage.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import PalletCard from '../../components/PalletCard/PalletCard';
 
 const PalletListPage = (props) => (
     // props.pallets
-    <div>PalletListPage</div>
+    <div>
+        <div>
+            <div>Pallets</div>
+            <div>Fruit: {props.species}</div>
+            <div>Variety: {props.variety}</div>
+            <div>Size: {props.size}</div>
+        </div>
+        <div>
+            {props.pallets.map((pallet, idx) => (
+                <PalletCard
+                    key={idx}
+                    pallet={pallet}
+                />
+            ))}
+        </div>
+    </div>
 );
 
 export default PalletListPage;
