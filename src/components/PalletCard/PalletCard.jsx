def10959265bb94f80dcd1742cf8b98d20939951
Wesley <wesleystedman@gmail.com>
2020-10-19 03:26:29 -0700

    Change pallet list from cards to table

--- src/components/PalletCard/PalletCard.jsx
+++ src/components/PalletCard/PalletCard.jsx
@@ -2,29 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PalletCard = (props) => (
-    <div className="shadow">
-        <div>
-            <div>Pallet ID: {props.pallet.palletID}</div>
-            <div>Status: {props.pallet.status}</div>
-            <div>Arrival Date: {new Date(props.pallet.arrivalDate).toLocaleDateString()}</div>
-            <div>Warehouse ID: {props.pallet.warehouseID}</div>
-            <div>Grower ID: {props.pallet.growerID}</div>
-            <div>Label Name: {props.pallet.labelName}</div>
-            <div>Box Count: {props.pallet.boxCount}</div>
-            <div>-----</div>
-            <div>Pack Date: {new Date(props.pallet.packDate).toLocaleDateString()}</div>
-            <div>Packaging: {props.pallet.packaging}</div>
-            <div>Repack: {props.pallet.repack}</div>
-            <div>Fumigated: {props.pallet.fumigated}</div>
-            <div>Boat: {props.pallet.boat}</div>
-            <div>Load: {props.pallet.load}</div>
-            <div>Returned: {props.pallet.isReturned}</div>
-        </div>
-        <div>
+    <tr>
+        <td>{props.pallet.palletID}</td>
+        <td>{props.pallet.status}</td>
+        <td>{new Date(props.pallet.arrivalDate).toLocaleDateString()}</td>
+        <td>{props.pallet.warehouseID}</td>
+        <td>{props.pallet.growerID}</td>
+        <td>{props.pallet.labelName}</td>
+        <td>{props.pallet.boxCount}</td>
+        <td>{new Date(props.pallet.packDate).toLocaleDateString()}</td>
+        <td>{props.pallet.packaging}</td>
+        <td>{props.pallet.repack && 'No'}</td>
+        <td>{props.pallet.fumigated}</td>
+        <td>{props.pallet.boat}</td>
+        <td>{props.pallet.load}</td>
+        <td>{props.pallet.isReturned ? 'Yes' : 'No'}</td>
+
+        {/* <td>
             <Link>Edit</Link>
             <Link>Delete</Link>
-        </div>
-    </div>
+        </td> */}
+    </tr>
 );
 
 export default PalletCard;
--- src/pages/PalletListPage/PalletListPage.jsx
+++ src/pages/PalletListPage/PalletListPage.jsx
@@ -10,14 +10,34 @@ const PalletListPage = (props) => (
             <div>Variety: {props.variety}</div>
             <div>Size: {props.size}</div>
         </div>
-        <div>
-            {props.pallets.map((pallet, idx) => (
-                <PalletCard
-                    key={idx}
-                    pallet={pallet}
-                />
-            ))}
-        </div>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Pallet ID</th>
+                    <th>Status</th>
+                    <th>Arrival Date</th>
+                    <th>Warehouse ID</th>
+                    <th>Grower ID</th>
+                    <th>Label Name</th>
+                    <th>Box Count</th>
+                    <th>Pack Date</th>
+                    <th>Packaging</th>
+                    <th>Repack</th>
+                    <th>Fumigated</th>
+                    <th>Boat</th>
+                    <th>Load</th>
+                    <th>Returned</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.pallets.map((pallet, idx) => (
+                    <PalletCard
+                        key={idx}
+                        pallet={pallet}
+                    />
+                ))}
+            </tbody>
+        </table>
     </div>
 );
 
